46a194d09216aa9c49a9a96c3f2cdec983761ac0
hamadavrgame <hamadavrgame@gmail.com>
2018-07-28 17:03:22 +0900

    DepthMask検証用サンプルプロジェクトをUP

---
+++ Assets/MiDA/test/scroll/scroll.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroll : MonoBehaviour {
+
+    [SerializeField]
+    private Transform cameraGr;
+
+    [SerializeField]
+    private Transform charGr;
+
+    [SerializeField]
+    private GameObject VRcontroller_R;
+
+    [SerializeField]
+    private Vector3 speedvec = Vector3.right;
+
+    [SerializeField]
+    private Transform route;
+
+    private Vector3 stage_dir;
+
+    [SerializeField]
+    private float speed_mul;
+
+	// Use this for initialization
+	void Start () {
+        float angleDir = route.eulerAngles.z * (Mathf.PI / 180.0f);
+        stage_dir = new Vector3(Mathf.Cos(angleDir), Mathf.Sin(angleDir), 0.0f);
+
+        angleDir = route.eulerAngles.z * (Mathf.PI / 180.0f);
+        speedvec = new Vector3(Mathf.Cos(angleDir), Mathf.Sin(angleDir), 0.0f);
+        speedvec *= speed_mul;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+        if (Input.GetKey(KeyCode.Z))
+        {
+            cameraGr.position -= speedvec;
+            charGr.position -= speedvec;
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            cameraGr.position += speedvec;
+            charGr.position += speedvec;
+        }
+    }
+}
---
+++ Assets/demo/material/DepthMask_sample01.shader
@@ -0,0 +1,18 @@
+﻿Shader "Unlit/DepthMask_sample01"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+
+	SubShader{
+		Pass
+		{
+			// Depth Testの実施 および Depth Bufferの更新
+			Zwrite On
+
+			// 色情報は更新しない
+			ColorMask 0
+		}
+	}
+}
